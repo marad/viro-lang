6b345866e7b1545b40d0b09047d1f8b13172e0ab
Marcin Radoszewski <moriturius@gmail.com>
2025-09-13 16:49:41 +0200

    Add test summary at the end

--- spec/init.lua
+++ spec/init.lua
@@ -1,9 +1,17 @@
 function RunTests(t)
+	local tests = 0
+	local success = 0
 	for name, test in pairs(t) do
 		print("--- " .. name .. " ---")
 		local ok, err = pcall(test)
-		if not ok then
+		tests = tests + 1
+		if ok then
+			success = success + 1
+		else
 			print(err)
 		end
 	end
+
+	print("========== Summary ==========")
+	print("All: " .. tests .. " Success: " .. success .. " Failed: " .. (tests - success))
 end
